c4b3344227af102afca32cbe181c7ab8a32fab63
Igor Vasiljevic <ivas@slurm.ttic.edu>
2016-04-13 16:44:03 -0500

    added coordinate

---
+++ coordinate.lua
@@ -0,0 +1,93 @@
+--Create the two coordinate tensors.
+
+function isint(n)
+  return n==math.floor(n)
+end
+
+function coordinate_x(x,y)
+dim_x = x
+dim_y = y
+
+coord = torch.Tensor(dim_x,dim_y):zero()
+center = (dim_x+1)/2
+
+coord[{dim_x,dim_y}] = 1
+coord[{1,dim_y}] = 1
+coord[{1,1}] = -1
+coord[{dim_x,1}] = -1
+
+function edge_line(lin)
+div = dim_x - center
+div = math.ceil(div)
+
+for i=2,dim_x-1 do
+prev = coord[{i-1,lin}]
+
+if i<center then 
+coord[{i,lin}] = prev - coord[{1,lin}]/(div)
+end
+
+if i>center then 
+if math.ceil(center) == i then prev = 0 end 
+coord[{i,lin}] = prev + coord[{1,lin}]/(div) 
+end
+end
+end 
+
+edge_line(1)
+edge_line(dim_y)
+
+for j = 1, dim_x do 
+for i = 2, dim_y-1 do
+diff = coord[{j,dim_y}] - coord[{j,1}]
+coord[{j,i}] = diff/(dim_y-1) + coord[{j,i-1}]
+end
+end
+
+return coord
+end
+--Y COORDINATE TENSOR
+
+function coordinate_y(x,y)
+dim_x = x
+dim_y = y
+
+coord = torch.Tensor(dim_x,dim_y):zero()
+center = (dim_y+1)/2
+
+coord[{dim_x,dim_y}] = -1
+coord[{1,dim_y}] = 1
+coord[{1,1}] = 1
+coord[{dim_x,1}] = -1
+
+function edge_line(lin)
+div = dim_y - center
+div = math.ceil(div)
+
+for i=2,dim_y-1 do
+prev = coord[{lin,i-1}]
+
+if i<center then 
+coord[{lin,i}] = prev - coord[{lin,1}]/(div)
+end
+
+if i>center then 
+if math.ceil(center) == i then prev = 0 end 
+coord[{lin,i}] = prev + coord[{lin,1}]/(div) 
+end
+end
+end 
+
+edge_line(1)
+edge_line(dim_x)
+
+for j = 1, dim_y do 
+for i = 2, dim_x-1 do
+diff = coord[{dim_x,j}] - coord[{1,j}]
+coord[{i,j}] = diff/(dim_x-1) + coord[{i-1,j}]
+end
+end
+
+return coord
+end
+
--- dataset.lua
+++ dataset.lua
@@ -1,7 +1,7 @@
-function load_data(filepath)
+--filePath = '/share/data/vision-greg/mlfeatsdata/unifiedsegnet/Torch/voc12-rand-all-val_GT.mat'
+--filePath = "/share/data/vision-greg/mlfeatsdata/CV_Course/voc12-val_GT.mat" end
 
-    filePath = '/share/data/vision-greg/mlfeatsdata/unifiedsegnet/Torch/voc12-rand-all-val_GT.mat'
-    if dataset=="val" then filePath = "/share/data/vision-greg/mlfeatsdata/CV_Course/voc12-val_GT.mat" end
+function load_data(filePath)
 
     loaded = matio.load(filePath)  --- you don't need to transpose if you use matio
     imlist = loaded.Imlist
