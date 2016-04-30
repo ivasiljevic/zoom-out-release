8ae0a829d9f8174a5dccfea4817f2197b203a3e8
Igor Vasiljevic <ivas@slurm.ttic.edu>
2016-04-30 00:03:09 -0500

    starting end-to-end

--- zoomoutclassifier.lua
+++ zoomoutclassifier.lua
@@ -1,8 +1,7 @@
 --Constructs classifier model.
 
-function zoomoutclassifier(origstride,nlabels,nhiddenunits,inputsize)
-
-    model = nn.Sequential()
+function zoomoutclassifier(mod,origstride,nlabels,nhiddenunits,inputsize)
+    model = mod
     model:add(nn.SpatialConvolutionMM(inputsize, nhiddenunits, 1, 1))
     model:add(nn.ReLU(true))
     model:add(nn.Dropout(0.5))
@@ -12,8 +11,8 @@ function zoomoutclassifier(origstride,nlabels,nhiddenunits,inputsize)
     model:add(nn.SpatialConvolutionMM(nhiddenunits, nlabels, 1, 1))
     model:add(nn.SpatialFullConvolution(nlabels,nlabels,origstride*2,origstride*2,origstride,origstride,origstride/2,origstride/2))
     W = Bilinearkernel(origstride*2,nlabels,nlabels)  -- initailization to bilinear upsampling
-    model.modules[8].weight= W
-    model.modules[8].bias:fill(0);
+    model.modules[83].weight= W
+    model.modules[83].bias:fill(0);
 
     criterion = cudnn.SpatialCrossEntropyCriterion()
     model:cuda()
