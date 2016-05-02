fea24630a1e1b9b9f79888eec4e11d222ac7b200
Mohammadreza Mostajabi <mostajabi@ttic.edu>
2016-05-02 13:46:32 -0500

    no message

--- Replicatedynamic.lua
+++ Replicatedynamic.lua
@@ -42,20 +42,21 @@ function Replicatedynamic:updateOutput(input)
 end
 
 function Replicatedynamic:updateGradInput(input, gradOutput)
-   input = input[1];
-   self.gradInput:resizeAs(input):zero()
-   local batchOffset = self.ndim and input:dim() > self.ndim and 1 or 0
+   input1 = input[1];
+   self.gradInput:resizeAs(input1):zero()
+   local batchOffset = self.ndim and input1:dim() > self.ndim and 1 or 0
    local rdim = self.dim + batchOffset
-   local sz = torch.LongStorage(input:dim()+1)
+   local sz = torch.LongStorage(input1:dim()+1)
    sz[rdim] = 1
-   for i = 1,input:dim() do
+   for i = 1,input1:dim() do
       local offset = 0
       if i >= rdim then
          offset = 1
       end
-      sz[i+offset] = input:size(i)
+      sz[i+offset] = input1:size(i)
    end
    local gradInput = self.gradInput:view(sz)
    gradInput:sum(gradOutput, rdim)
+   self.gradInput={self.gradInput,torch.Tensor():typeAs(input[2]):resizeAs(input[2]):zero()}
    return self.gradInput
 end
