b9763ad8e7167b2c81a770790f2b048374902f9c
Igor Vasiljevic <ivas@slurm.ttic.edu>
2016-08-29 17:42:17 -0500

    cleanup

---
+++ coco/datatest.lua
@@ -0,0 +1,27 @@
+require 'image'
+matio = require 'matio'
+
+coco_dir = "/share/data/vision-greg/coco/train2014/"
+coco_voc = "/share/data/vision-greg/coco/gt-voc/"
+
+tag_dir = "/share/data/vision-greg/mlfeatsdata/voc_tags.t7"
+
+--dat = io.open("/share/data/vision-greg/coco/train.txt","r")
+dat = io.open("test.txt","r")
+io.input(dat)
+temp = {}
+for i = 1,66843 do
+table.insert(temp, io.read())
+end
+io.close(dat)
+
+--torch.save(temp, "coco.t7")
+img=image.load(coco_dir..string.sub(temp[1],1,-5)..".jpg")
+temp_gt=matio.load("/share/data/vision-greg/coco/gt-class/"..temp[1])
+gt = temp_gt.groundTruth[1].Segmentation
+--temp_gt = matio.load("/share/data/vision-greg/coco/gt-voc/"..temp[index0])
+--im = image.load(coco_dir..string.sub(temp[index0],1,-5)..".jpg")
+
+--print(temp[1])
+--torch.save(temp, "coco.t7")
+
