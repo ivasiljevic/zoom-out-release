1ffd0481c61709e19ab94ea4e587b33b09781bbe
Igor Vasiljevic <ivas@slurm.ttic.edu>
2016-09-12 10:38:12 -0500

    tutorial

--- tutorial/example.lua
+++ tutorial/example.lua
@@ -28,9 +28,7 @@ im_proc = torch.Tensor(1,3,im_proc_temp:size()[2],im_proc_temp:size()[3])
 im_proc[{{1},{},{},{}}] = im_proc_temp
 pred = model:forward(im_proc:cuda())
 
-gt_temp = nil
-concatfeats = nil
 im_rescale = image.scale(pred[1]:float(),im:size()[3],im:size()[2],"bilinear")
 C,D = torch.max(im_rescale,1)
-gt = D:squeeze():double()
-image.save("mask.png", gt)
+label = D:squeeze():double()/21
+image.save("mask.jpg", label)
